cde18dab62c2793bfddd8cc1aad2071738a73339
ricardo-1795 <rm658748@gmail.com>
2023-07-11 12:22:45 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinal-23am/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_23am.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_23am.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+
+        {
+            options.UseMySQL("server = localhost; database=ProyectoDb23Am; user=root; password=;");
+
+
+                }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+    }
+}
